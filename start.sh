106ad69860e5160a6f2e10cacda1888d833375b1
Kaggggggga <kaga.fromearth@gmail.com>
2019-06-08 15:34:09 +0800

    chore: update examples

--- start.sh
+++ start.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env sh
 
+## generate host key
 /usr/bin/ssh-keygen -A
-ssh-keygen -t rsa -b 4096 -f  /etc/ssh/ssh_host_key
+yes y | ssh-keygen -t rsa -b 4096 -f  /etc/ssh/ssh_host_key -N ""
 
+## generate client key
+if [ ! -z "$SSH_GEN_CLIENT_KEY" ]; then
+    yes y | ssh-keygen -t rsa -b 4096 -f  /root/.ssh/id_rsa -N ""
+    echo "" >>  /root/.ssh/authorized_keys
+    cat /root/.ssh/id_rsa.pub >> /root/.ssh/authorized_keys
+fi
+syslogd
 /usr/sbin/sshd -D
